c1f1c9f8e6b9a39585d27148bf1c056a69f10134
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-02-05 06:28:35 +0900

    Set icon path using <IconSettings>

--- public/index.js
+++ public/index.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
 import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css';
+import IconSettings from '@salesforce/design-system-react/components/icon-settings';
 
 import configureStore from './store/configureStore';
 import App from './containers/App';
@@ -14,11 +15,20 @@ import './styles/index.scss';
 const store = configureStore();
 
 const rootEl = document.getElementById('root');
+const LDS_ROOT = 'assets/lds';
 
 render(
   <AppContainer>
     <Provider store={store}>
-      <App />
+      <IconSettings
+        standardSprite={`${LDS_ROOT}/icons/standard-sprite/svg/symbols.svg`}
+        utilitySprite={`${LDS_ROOT}/icons/utility-sprite/svg/symbols.svg`}
+        actionSprite={`${LDS_ROOT}/icons/action-sprite/svg/symbols.svg`}
+        doctypeSprite={`${LDS_ROOT}/icons/doctype-sprite/svg/symbols.svg`}
+        customSprite={`${LDS_ROOT}/icons/custom-sprite/svg/symbols.svg`}
+      >
+        <App />
+      </IconSettings>
     </Provider>
   </AppContainer>,
   rootEl,
@@ -29,7 +39,15 @@ if (module.hot) {
     render(
       <AppContainer>
         <Provider store={store}>
-          <App />
+          <IconSettings
+            standardSprite={`${LDS_ROOT}/icons/standard-sprite/svg/symbols.svg`}
+            utilitySprite={`${LDS_ROOT}/icons/utility-sprite/svg/symbols.svg`}
+            actionSprite={`${LDS_ROOT}/icons/action-sprite/svg/symbols.svg`}
+            doctypeSprite={`${LDS_ROOT}/icons/doctype-sprite/svg/symbols.svg`}
+            customSprite={`${LDS_ROOT}/icons/custom-sprite/svg/symbols.svg`}
+          >
+            <App />
+          </IconSettings>
         </Provider>
       </AppContainer>,
       rootEl,
