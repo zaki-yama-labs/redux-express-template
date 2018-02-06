4c3368151beb1ffccc0075ea0fe2b1e9eed42917
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-02-07 01:07:28 +0900

    Unify .babelrc and public/.babelrc

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
